e9736d7266278c22a400ef377e26f39a5ff6195b
pranav <pranav8iisc@gmail.com>
2014-10-15 12:38:42 +0530

    basic version of sample program to read idx dataset

--- Samples/idxReaderSample.cpp
+++ Samples/idxReaderSample.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
+#include <string>
 #include "../idxDatasetReader.h"
+
+
+using namespace std;
+
 
 int main()
 {
+	IdxDatasetReader idxReader;
+	string fileName;
 	
+	cout << "Please enter the filename";
+	cin >> fileName;
+
+	idxReader.getDataset(fileName);
+	idxReader.saveJPEG();'
+
 	return 0;
 }
